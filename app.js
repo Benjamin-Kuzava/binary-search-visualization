1476d39a334b676619249eaa09b1f5fe7c2b75a0
benjamin kuzava <benjamin@kuzav1bj-2.local>
2020-12-21 13:20:03 -0500

    Generate books on shelves

--- app.js
+++ app.js
@@ -30,8 +30,36 @@ async function GetBooks(genre) {
   console.log(bookArray)
 }
 
-// async function getBookCover(book) {
-//   const responses = await axios.get(url)
-// }
-
 // GetBooks('horror');
+
+// Create Books
+function GenerateBooks(num) {
+  // counter for column position
+  let columnPosition = 0; 
+  for (let i = 0; i < num; i++) {
+    const gridContainer = document.querySelector('.grid-container')
+    const bookDiv = document.createElement('div');
+    bookDiv.classList.add('book')
+    bookDiv.setAttribute("id", `i${i}`);
+    bookDiv.textContent = 'text';
+
+    // If i has reached a shelf breakpoint, reset column counter 
+    if (i == Math.floor(num * (2 / 3)) || i == Math.floor(num * (1 / 3))) {
+      columnPosition = 0;
+    } 
+    bookDiv.style.gridColumn = columnPosition + 4;
+
+    // If a shelf has filled, move to next shelf
+    if (i >= num * (2 / 3)) {
+      bookDiv.style.gridRow = 18;
+    } else if (i >= num * (1 / 3)) {
+      bookDiv.style.gridRow = 13;
+    } else {
+      bookDiv.style.gridRow = 8;
+    }
+    gridContainer.append(bookDiv);
+    columnPosition++;
+  }
+}
+
+GenerateBooks(87);
